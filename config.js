670924ec23f66d8e074d7c339ff5c8538727296a
mxzzz505 <farajimxzzz@gmail.com>
2026-07-24 01:32:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212666666666;
+global.pairingNumber = 22220330028;
 global.owner = [
-  ['212717457920', 'noureddine Ouafy', true],
+  ['22220330028', 'MXZ', true],
   ['', 'Owner 2', true],
 ];
 
-global.namebot = 'gaff ai';
-global.author = 'Noureddine ouafy';
-global.source = 'https://chat.whatsapp.com/Hp3R0WWD5G8Li9HKBU6fn3';
+global.namebot = 'MXZ';
+global.author = 'MXZ';
+global.source = 'https://t.me/+ceQVknPmYnRjMjRk';
 
 global.wait = 'Loading... | جاري الانتظار';
 global.eror = 'There is an error... | وقع خطأ';
